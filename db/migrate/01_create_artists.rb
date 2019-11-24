0e94623df229075476cffdabc8daed52e267f373
Baktiar Miah <baktiar.m@gmail.com>
2019-11-24 02:00:05 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,14 +1,14 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      title TEXT,
-      length INTEGER
-      )
-    SQL
-    ActiveRecord::Base.connection.execute(sql)
-  end
+  # def self.create_table
+  #   sql = <<-SQL
+  #     CREATE TABLE IF NOT EXISTS songs (
+  #     id INTEGER PRIMARY KEY,
+  #     title TEXT,
+  #     length INTEGER
+  #     )
+  #   SQL
+  #   ActiveRecord::Base.connection.execute(sql)
+  # end
 
   def change
     create_table :artists do |t|
