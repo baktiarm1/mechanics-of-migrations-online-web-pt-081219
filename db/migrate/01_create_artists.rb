b1eeb4e5576ddf08abdb3fa380fbb04ebe222308
Baktiar Miah <baktiar.m@gmail.com>
2019-11-24 02:02:04 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -11,14 +11,14 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   SQL
     ActiveRecord::Base.connection.execute(sql)
   end
-
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+  # 
+  # def change
+  #   create_table :artists do |t|
+  #     t.string :name
+  #     t.string :genre
+  #     t.integer :age
+  #     t.string :hometown
+  #   end
   end
 
 end
